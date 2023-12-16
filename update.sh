db7052281c20d8c4b1d522bf3caaee863813afe6
quanqiubianlen <1226216957@qq.com>
2023-12-16 22:01:23 +0800

    add update overlay shellscript

---
+++ update.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+openwrt_dir=/home/zhang/workspace/openwrt
+
+cd ${openwrt_dir}
+
+git pull
+if (( $? != 0 ))
+then
+    echo openwrt git pull error, return
+    exit
+fi
+cd -
+
+rm -rf ./overlay/*
+mkdir -p ./overlay/include
+mkdir -p ./overlay/target/linux/
+
+cp -arf ${openwrt_dir}/include/kernel-5.15 ./overlay/include/
+cp -arf ${openwrt_dir}/target/linux/generic ./overlay/target/linux/
+cp -arf ${openwrt_dir}/target/linux/ramips ./overlay/target/linux/
+
+cd ./overlay
+
+git add .
+if (( $? != 0 ))
+then
+    echo git add error, retrun
+    exit
+fi
+
+time=$(date "+%Y-%m-%d %H:%M:%S")
+commit_mgs="update overlay at ${time}"
+git commit -m "${commit_mgs}"
+if (( $? != 0 ))
+then
+    echo git commit error
+    git restore .
+    exit
+fi
+
+git push
+if (( $? != 0 ))
+then
+    echo git push error, git reset
+    git reset --hard HAED
+    exit
+else 
+    echo update overlay success.
+fi
+
+cd -
